54c868645999c702e38d0ff3a123bd737de0e412
Jairo Mac <jhonjairotorres75@gmail.com>
2023-11-19 16:50:51 -0500

    test_normality

--- buscandoelmodelo.R
+++ buscandoelmodelo.R
@@ -13,7 +13,7 @@ if (!exists("datos_arreglados")) {
 
 transformations <- c("none","log","square", "inverse", "sqrt", "cube", "reciprocal_squared", "cbrt", "standardize", "normalize")
 # c("presupuesto", "edad", "estrato", "libros_anio")
-vars <- c("presupuesto","edad", "estrato")
+vars <- c("presupuesto","edad", "estrato", "libros_anio")
 # c("genero_dummy")
 dicotomicas_vars <- c("genero_dummy")
 data_original <- datos_arreglados[, c(vars, dicotomicas_vars)]
@@ -22,7 +22,7 @@ cont = 1
 for (i in 1:nrow(result_combinations)) {
   combination <- combination <- sapply(result_combinations[i, ], as.character)
   model <- generate_model(data_original, combination, vars, dicotomicas_vars)
-  if (test_model_significance_rlm(model) && test_homoscedasticity(model)){
+  if (test_normality(model) && test_model_significance_rlm(model) && test_homoscedasticity(model)){
     print(summary(model))
     print(combination)
     break
